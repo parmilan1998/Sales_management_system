bd836fad1995e4f25059148176fab99e072ac2e4
YathuPiraba <yathavan1407@gmail.com>
2024-06-14 10:26:58 +0530

    removed category folders

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const categoryRoute = require('./routes/categoryRoute');
 const productRoute = require('./routes/productRoute');
 const bodyParser = require('body-parser');
 
@@ -13,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/v1/category', categoryRoute);
+
 app.use('/api/v1/product', productRoute);
 
 const PORT = process.env.PORT || 5000;
