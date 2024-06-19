049175d9326dbe01439cd839e4a0812dbbcdbe5b
YathuPiraba <yathavan1407@gmail.com>
2024-06-19 15:26:40 +0530

    completed sales and purchase update function

--- server/routes/salesRoute.js
+++ server/routes/salesRoute.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const{createSales} = require ("../controllers/salesController")
+const{createSales,getAllSales,updateSales,deleteSales, querySales} = require ("../controllers/salesController")
 
 router.post("/", createSales)
+router.get("/list",getAllSales)
+router.put("/:id",updateSales)
+router.get("/query",querySales)
+router.delete("/:id",deleteSales)
+
 
 module.exports = router
