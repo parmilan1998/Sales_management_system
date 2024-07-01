7e0daa0d4c4a289080e607001fcb2824f5da1f55
parmilan1998 <parmilancs2024@gmail.com>
2024-07-01 15:26:26 +0530

    product sorting, fixed update error, view in popUp and ant design delete conformation

---
+++ client/src/Components/Products/SingleProductPopUp.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+const SingleProductPopUp = ({ rowData }) => {
+  const baseUrl = "http://localhost:5000/public/products";
+
+  return (
+    <div>
+      <div className="overflow-hidden rounded bg-gray-200 text-slate-500 shadow-none shadow-slate-200 font-poppins">
+        <figure>
+          <img
+            src={`${baseUrl}/${rowData.imageUrl}`}
+            alt="card image"
+            className="aspect-video w-full"
+          />
+        </figure>
+        <div className="p-6">
+          <header className="mb-4 space-y-2">
+            <h3 className="text-xl font-medium text-slate-700">
+              {rowData.productName}
+            </h3>
+            <div className="flex justify-between items-center">
+              <p className="text-base text-slate-500">
+                Price: Rs.{rowData.unitPrice}
+              </p>
+              <p className="text-base text-slate-500">
+                Stock: {rowData.totalQuantity}
+              </p>
+            </div>
+            <p className="text-base text-slate-500">
+              Category: Rs.{rowData.categoryName}
+            </p>
+          </header>
+          <p className="text-md">Description: {rowData.productDescription}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleProductPopUp;
