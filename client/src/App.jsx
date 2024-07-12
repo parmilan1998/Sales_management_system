ed6a347c9cb3f4206e98055b459bd0543a4dda22
YathuPiraba <yathavan1407@gmail.com>
2024-07-12 14:48:40 +0530

    layout problem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "./Layout/RootLayout";
 import Category from "./Pages/Category/Category";
-import "./App.css";
 
 import Dashboard from "./Pages/Dashboard";
 import Home from "./Components/Home";
--- client/src/Layout/RootLayout.jsx
+++ client/src/Layout/RootLayout.jsx
@@ -5,15 +5,12 @@ import Navbar from "../Components/Navbar";
 
 const RootLayout = () => {
   return (
-    <div className="flex flex-row h-screen ">
-      <div className="fixed h-full z-50">
+    <div className="grid grid-cols-4 bg-slate-200">
+      <div className="col-span-1">
         <Sidebar />
       </div>
-      <div className="flex-1 flex flex-col overflow-hidden lg:pl-72">
-        {/* <Navbar /> */}
-        <div className="flex-1 overflow-y-scroll no-scrollbar bg-gray-200 px-4 lg:py-12 py-20">
-          <Outlet />
-        </div>
+      <div className="col-span-3 my-3 bg-gray-200">
+        <Outlet />
       </div>
     </div>
   );
